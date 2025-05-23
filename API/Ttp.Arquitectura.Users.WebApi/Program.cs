46697c4519f139ff83410cbb218dabef2cf2a2f5
cesarleon2024 <cesarleon2024@gmail.com>
2025-05-23 18:00:26 -0400

    Add .gitignore file and remove bin/obj folders from git tracking

--- API/Ttp.Arquitectura.Users.WebApi/Program.cs
+++ API/Ttp.Arquitectura.Users.WebApi/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Ttp.Arquitectura.Users.Application.Commands;
+using Ttp.Arquitectura.Users.Application.Queries;
+using Ttp.Arquitectura.Users.Domain.Interfaces.Repository;
+using Ttp.Arquitectura.Users.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UsersContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("users-db")));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<AddUserHandler>();
+builder.Services.AddScoped<GetUsersHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
